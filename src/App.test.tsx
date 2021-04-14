33de8d946b7806e72830b063b0a76399955fbb20
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-14 17:43:54 +0200

    should install craco (does not work on hackerrank)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
-import {Todos} from './component/Todos';
 
 it("should match snapshot", () => {
-    const {container} = render(<App/>);
-    expect(container).toMatchSnapshot();
+    const {asFragment} = render(<App/>);
+    expect(asFragment()).toMatchSnapshot();
 });
